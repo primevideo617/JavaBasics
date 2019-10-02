80e2e449157adf42c19580eb932568ea2d30c0a6
primevideo617 <55767025+primevideo617@users.noreply.github.com>
2019-10-02 13:17:21 +0100

    modified to test git pull

--- coderpad/src/main/java/com/learn/coderpad/HelloWorld.java
+++ coderpad/src/main/java/com/learn/coderpad/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Relearning everything !!");
 		System.out.println("Relearning everything for new future!!");
+		System.out.println("Test Git PUll!!");
 	}
 
 }
